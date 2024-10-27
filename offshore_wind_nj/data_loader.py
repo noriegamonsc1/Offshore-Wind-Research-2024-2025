2e2a18a9ba8d613e3ccd43da3fda36dfd51532b9
Cristian Noriega <noriegamonsc1@montclair.edu>
2024-10-27 13:26:49 -0400

    added data cleaning as modules

--- offshore_wind_nj/data_loader.py
+++ offshore_wind_nj/data_loader.py
@@ -1,45 +1,49 @@
-# data_loader.py
-
 import numpy as np
 from pathlib import Path
 from loguru import logger
 import offshore_wind_nj.config as config
 import re
 from datetime import datetime
-from typing import Tuple
+from offshore_wind_nj.data_cleaning import fill_zeros
+from typing import List, Tuple
 
-# Create a list of all .npz files in the processed data directory
-data_files = list(config.PROCESSED_DATA_DIR.glob('*.npz'))
-
-# Global variable to store all loaded arrays
-all_arrays = []
-
-def load_data(input_files):
+def load_data(input_files: List[Path]) -> Tuple[List[Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]], List[Path]]:
     """
     Load and preprocess data from a list of input .npz files.
     
     Parameters:
-        input_files (list): List of paths to the input .npz files.
+        input_files (list of Path): Paths to the input .npz files.
+        
+    Returns:
+        Tuple: all_arrays (List of tuples with loaded data), cleaned_files (List of files with zeros).
     """
-    global all_arrays  # Declare that we are using the global variable
-    all_arrays = []  # Clear the list before loading new data
+    all_arrays = []
+    cleaned_files = []
     
     for input_file in input_files:
-        # logger.info(f"Loading data from {input_file}...")
         with np.load(input_file) as data:
             owi_speed = data['owiSpeed']
             owi_dir = data['owiDir']
             lat = data['lat']
             lon = data['lon']
-            all_arrays.append((owi_speed, owi_dir, lat, lon))
-        # logger.success(f"Data loaded successfully from {input_file}.")
 
-def load_single_data(input_file):
+            # Check for zero-speed points
+            if np.any(owi_speed == 0):
+                cleaned_files.append(input_file)
+                clean_data = (owi_speed, owi_dir, lat, lon)
+                filled_data = fill_zeros(clean_data)  # Fill zeros
+                all_arrays.append(filled_data)
+            else:
+                all_arrays.append((owi_speed, owi_dir, lat, lon))
+    
+    return all_arrays, cleaned_files
+
+def load_single_data(input_file: Path) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Load and preprocess data from a single input .npz file.
     
     Parameters:
-        input_file (str): Path to the input .npz file.
+        input_file (Path): Path to the input .npz file.
         
     Returns:
         tuple: A tuple containing (owi_speed, owi_dir, lat, lon).
@@ -55,37 +59,30 @@ def load_single_data(input_file):
 
 def extract_datetime_from_filename(file_path: Path) -> Tuple[str, str, str]:
     """
-    Extracts the filename and formats the start and end datetime from a given file path.
+    Extracts filename and formats start and end datetime from a given file path.
     
     Args:
-        file_path (Path): The full path to the file.
-
-    Returns:
-        Tuple[str, str, str]: The filename without the extension, the start datetime as 'YYYY-MM-DD HH:MM:SS',
-                              and the end datetime as 'YYYY-MM-DD HH:MM:SS'.
-    """
-    # Extract the filename without the extension
-    filename = Path(file_path).stem
+        file_path (Path): Full path to the file.
     
-    # Split filename into parts based on underscore
+    Returns:
+        Tuple[str, str, str]: Filename without extension, start date, start and end times.
+    """
+    filename = file_path.stem
     parts = re.split('_', filename)
     
     try:
-        # Convert the start and end timestamp parts to datetime objects and format them
         date = datetime.strptime(parts[5], '%Y%m%dT%H%M%S').strftime('%Y-%m-%d')
         start_time = datetime.strptime(parts[5], '%Y%m%dT%H%M%S').strftime('%H:%M:%S')
         end_time = datetime.strptime(parts[6], '%Y%m%dT%H%M%S').strftime('%H:%M:%S')
     except (IndexError, ValueError) as e:
-        raise ValueError(f"Filename '{filename}' does not contain valid timestamps or has an unexpected format.") from e
+        logger.error(f"Filename '{filename}' does not contain valid timestamps or has an unexpected format.")
+        raise ValueError("Filename format issue") from e
     
     return filename, date, start_time, end_time
 
-
 if __name__ == "__main__":
-    print(f"{__name__} run as a module")
+    print("Module run independently.")
 else:
     data_files = list(config.PROCESSED_DATA_DIR.glob('*.npz'))
-    
-    # Call the load_data function to populate all_arrays
-    load_data(data_files)
-    print(f'There are {len(data_files)} files')
+    all_arrays, cleaned_files = load_data(data_files)
+    print(f'{len(cleaned_files)} files required cleaning.')
